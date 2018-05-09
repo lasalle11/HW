15b3a7b42d60d0f9f299810428a071529584b39c
LL <LL@DESKTOP-7PN8UA3.attlocal.net>
2018-05-09 15:57:51 -0400

    Initial upload

---
+++ Lesson1/src/Addition.java
@@ -0,0 +1,35 @@
+// Addition.java
+// Addition program that displays the sum of two numbers.
+import java.util.Scanner; // program uses class Scanner
+
+public class Addition 
+{
+   // main method begins execution of Java application
+   public void run()
+   {
+      // create a Scanner to obtain input from the command window
+      Scanner input = new Scanner(System.in);
+
+      int number1; // first number to add
+      int number2; // second number to add
+      int sum; // sum of number1 and number2
+      
+      char l = 66;
+
+      System.out.print("Enter first integer: "); // prompt 
+      number1 = input.nextInt(); // read first number from user
+
+      System.out.print("Enter second integer: "); // prompt 
+      number2 = input.nextInt(); // read second number from user
+
+      sum = number1 + number2; // add numbers, then store total in sum
+
+      System.out.printf("Sum is %d%n", sum); // display sum
+      
+      System.out.print(l);
+      
+      
+      input.close();
+   } // end method main
+} // end class Addition
+
---
+++ Lesson1/src/run.java
@@ -0,0 +1,10 @@
+
+public class run
+{
+ public static void main (String args [])
+ {
+	 Addition add = new Addition();
+		add.run();
+ }
+	
+}
